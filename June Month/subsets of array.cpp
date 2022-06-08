90223a88f909092a53eac159ce15921e059b7ecd
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2022-06-08 16:19:39 +0530

    Create subsets of array.cpp

---
+++ June Month/subsets of array.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int decimalToBinary(int n){
+    int ans =0;
+    int power = 1;
+    while(n!=0){
+        int r = n%2;  // example = 10
+        n = n/2;
+        ans += (r*power);
+        power *= 10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+
+    for(int i =0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int subsets = pow(2, n);
+    for(int i=0; i<subsets; ++i){
+        int bn = decimalToBinary(i);
+        int div = pow(10,n-1);
+
+        for(int j=0; j<n; ++j){
+            int q = bn/div;
+            int r = bn%div;
+
+            if(q==0){
+                cout << "-\t";
+            }else{
+                cout << arr[j] << "\t";
+            }
+
+            bn = r;
+            div = div/10;
+        }
+        cout << endl;
+    }
+}
