8991b69ffd31ab2af6ce6982cf44af651e57cc2e
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2022-08-02 13:44:25 +0530

    Update Kth smallest element.cpp

--- Array/Kth smallest element.cpp
+++ Array/Kth smallest element.cpp
@@ -7,8 +7,25 @@ class Solution{
     // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
+     /*
         int n = l+r+1;
         sort(arr, arr + n);  // nlogn TC
         return arr[k-1];
+        */
+        
+        // using set stl as it runs on the average TC of O(logn)
+        // we are using set here because in the problem statement it is given that all elements are distinct
+        
+        int n = l+r+1;
+        
+        set <int> s(arr, arr +n);
+        
+        set<int> :: iterator it1 = s.begin();
+        
+        advance(it1, k-1);
+        
+    //    int ans = *it1;
+        
+        return *it1;
     }
 };
