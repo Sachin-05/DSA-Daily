6dbafd44f5b0acb3916b57d08e3a364c2f038553
Sachin-05 <51017352+Sachin-05@users.noreply.github.com>
2022-12-18 15:37:25 +0530

    Update Distribute in Circle.cpp

--- Math/Distribute in Circle.cpp
+++ Math/Distribute in Circle.cpp
@@ -1,7 +1,14 @@
 int Solution::solve(int A, int B, int C) {
+   /*
     int t = A%B;
     
     int ans = t+C-1;
     
     return ans%B;
+    */
+    
+    // learn the modular expression
+    
+    return (A+C)%B-1;
+            
 }
